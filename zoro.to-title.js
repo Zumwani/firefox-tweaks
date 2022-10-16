b68310e7dd588eda3407d43367f3cdcca618bae5
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-10-16 22:35:26 +0200

    Update zoro.to-title.js

--- zoro.to-title.js
+++ zoro.to-title.js
@@ -5,15 +5,21 @@
 // @author			zumwani
 // @namespace       http://github.com/zumwani
 // @downloadURL		https://raw.github.com/zumwani/firefox-tweaks/main/zoro-title.js
-// @match           https://*.zoro.to/*
+// @match           https://*.zoro.to/watch/*
 //
 // @version         1.0
 // @updateURL		https://raw.github.com/zumwani/firefox-tweaks/zoro-title.json
 //
-// @run-at			document-start|document-end
+// @run-at			document-start
 // @unwrap
 // ==/UserScript==
 
 setInterval(() => {
-    //film-name
+
+    const title = document.querySelector(".film-name").innerText;
+    const currentEpisode = document.querySelector(".ssl-item.ep-item.active")?.getAttribute("data-number");
+    const maxEpisode = document.querySelector(".ssl-item.ep-item:last-child")?.getAttribute("data-number");
+
+    document.title = `${title} [${currentEpisode ?? "?"}/${maxEpisode ?? "?"}]`;
+
 }, 1000);
