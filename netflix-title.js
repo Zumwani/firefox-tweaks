dbbebe642c7640de8a5640f41eb669f9adb77afc
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-10-16 22:07:36 +0200

    Create netflix-title.js

---
+++ netflix-title.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          	netflix-title
+// @description     Fixes titles on netflix
+//
+// @author			zumwani
+// @namespace       http://github.com/zumwani
+// @downloadURL		https://raw.github.com/zumwani/firefox-tweaks/main/netflix-title.js
+// @match           https://*.netflix.com/*
+//
+// @version         1.0
+// @updateURL		https://raw.github.com/zumwani/firefox-tweaks/netflix-title.json
+//
+// @run-at			document-start|document-end
+// @unwrap
+// ==/UserScript==
+
+setInterval(() => {
+    
+}, 1000);
