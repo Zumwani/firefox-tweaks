e3a4b54be643f2f1e77cf426b1fe9084704fadf2
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-10-16 22:07:39 +0200

    Create zoro.to-title.js

---
+++ zoro.to-title.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          	zoro-title
+// @description     Fixes titles on zoro.to
+//
+// @author			zumwani
+// @namespace       http://github.com/zumwani
+// @downloadURL		https://raw.github.com/zumwani/firefox-tweaks/main/zoro-title.js
+// @match           https://*.zoro.to/*
+//
+// @version         1.0
+// @updateURL		https://raw.github.com/zumwani/firefox-tweaks/zoro-title.json
+//
+// @run-at			document-start|document-end
+// @unwrap
+// ==/UserScript==
+
+setInterval(() => {
+    
+}, 1000);
