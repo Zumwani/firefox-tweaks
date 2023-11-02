3f942469e1fa60f786361564f514b5c583392c2c
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2023-11-02 11:06:12 +0100

    Update symlink.bat

--- symlink.bat
+++ symlink.bat
@@ -1,8 +1,6 @@
 @echo off
 for /D %%G in ("%AppData%\Mozilla\Firefox\Profiles\*") DO ( 
 
-echo %%~nxG
-
 rmdir "%AppData%\Mozilla\Firefox\Profiles\%%~nxG\chrome"
 mklink /d "%AppData%\Mozilla\Firefox\Profiles\%%~nxG\chrome" "%cd%\chrome"
 
